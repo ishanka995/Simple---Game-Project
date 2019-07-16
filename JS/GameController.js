f15c837e1f3e10fbcb9a931b0ea18210b078e65a
ishanka995 <ishankaabeysingha@gmail.com>
2019-07-16 15:46:03 +0530

    bird and bat main controllers

--- JS/GameController.js
+++ JS/GameController.js
@@ -9,4 +9,30 @@ $(function () {
     var pole_1 = $('#pole_1');
     var pole_2 =$('#pole_2');
     var restart_btn = $('#restart_btn');
+
+    var container_width = parseInt(container.width());
+    var container_height = parseInt(container.height());
+    var pole_initial_position = parseInt(pole.css('left'));
+    var pole_width = parseInt(pole.width());
+    var bird_left = parseInt(bird.css('left'));
+    var bird_height = parseInt(bird.height());
+    var bird_width = parseInt(bird.width());
+
+    var go_up = false;
+    var go_down = false;
+    var game_over = false;
+
+    var bleft;
+    var pleft;
+
+
+    var bird_go = 'down';
+    var bird_right_left = false;
+
+    var move_right1 = false;
+    var move_left1 = false;
+
+    var who_won;
+
+
 })
